2f98e5e3018a5f8106bd90514477b3dba1106c7c
Frederik Nygaard Svendsen <fns@hungry.dk>
2020-01-20 17:41:14 +0100

    [HG-197] deleted leftover files from revert to commit a6ecc6ec05344f3a9a71207ca0b41bedf136289a

--- filters.py
+++
@@ -1,9 +0,0 @@
-import logging
-from . import local, NO_REQUEST_ID
-
-
-class RequestIDFilter(logging.Filter):
-
-    def filter(self, record):
-        record.request_id = getattr(local, 'request_id', NO_REQUEST_ID)
-        return True
